141c9e686b783454a610136be7f99ac8c32a43fc
mmy <15224924822@163.com>
2023-08-18 22:05:29 +0800

    点击欢迎面板显示浮动照片卡

--- src/components/FlowBorder/index.jsx
+++ src/components/FlowBorder/index.jsx
@@ -1,14 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './index.less';
-export default function index({width, height}) {
+export default function FlowBorder({width, height, layerImgUrl}) {
   const areaWidth = width ?? 300;
-  const areaHeight = height ?? 300;
+  const areaHeight = height ?? 220;
   return (
     <div className='flow-border'
       style={{width: areaWidth, height: areaHeight}}
     >
       <div className="box">
+        <div className="layer" style={{
+          backgroundImage: `url(${layerImgUrl})`
+        }}></div>
       </div>
     </div>
   )
