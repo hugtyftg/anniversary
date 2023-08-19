4cb359ee736a4ec6c23889e7befb5fdd5ee37242
mmy <15224924822@163.com>
2023-08-19 18:04:46 +0800

    添加card文字部分

--- src/components/GradientBorder/index.jsx
+++ src/components/GradientBorder/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.less';
-export default function index({width, height}) {
+export default function index({width, height, children}) {
   const areaWidth = width ?? 300;
   const areaHeight = height ?? 300;
   return (
@@ -8,7 +8,7 @@ export default function index({width, height}) {
       style={{width: areaWidth, height: areaHeight}}
     >
       <div className="box">
-      
+        {children}
       </div>
     </div>
   )
